477159ba37513714c8966233cd9a9510896959af
Zakaria Kerkeb <hikkary@MacBook-Pro-de-Zakaria.local>
2021-01-29 19:13:29 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <Text>BoilerPlate</Text>
+      </SafeAreaView>
+    </>
+  )
+}
+
+export default App
